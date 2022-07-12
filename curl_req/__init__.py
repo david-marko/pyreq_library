49ced696f1abc927fae5d52251986abb327d22fd
dmarkox <88544099+dmarkox@users.noreply.github.com>
2022-07-12 07:35:27 +0300

    Update __init__.py

---
+++ curl_req/__init__.py
@@ -0,0 +1,20 @@
+
+
+class requests:
+    def __init__(self) -> None:
+        pass
+
+    def get(self, url, **kwargs):
+        # Params, Headers, Cookies, Authentication, Proxies, Allow Redirects, Timeout
+        pass
+
+    def post(self, url, **kwargs):
+        # Params, Headers, Cookies, Authentication, Proxies, Allow Redirects, Timeout
+        pass
+
+    @staticmethod
+    def session(self):
+        pass
+
+    def exception_handler(self):
+        pass
